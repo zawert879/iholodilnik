84f3684c71fc878c55639a8facc557af7be1ac84
IGorRozhkov <zawert23879@gmail.com>
2017-11-03 19:10:46 +0300

    bufFix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('photo');
+            $table->string('photo')->default('default.cvg');
             $table->rememberToken();
             $table->timestamps();
         });
