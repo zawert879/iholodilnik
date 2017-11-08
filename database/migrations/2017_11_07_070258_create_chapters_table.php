f44502999d92ca76f3a64f39dafc6c9bdb022f28
IGorRozhkov <zawert23879@gmail.com>
2017-11-08 03:23:27 +0300

    add recipes

---
+++ database/migrations/2017_11_07_070258_create_chapters_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChaptersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chapters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_recipe');
+            $table->text('text');
+            $table->string('photo')->nullable();
+            $table->integer('timer')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chapters');
+    }
+}
